fda673d574ec8afdfe5bdd9fa4267bbce3fdea4e
qh7574 <1943197365@qq.com>
2025-12-17 14:23:03 +0800

    主要改进： 1. 版本更新至 3.1.2； 2. 优化 BottomSheet 主题配置，支持动态取色，移除布局中冗余背景设置； 3. 调整胶片颗粒设置底部按钮栏样式及水印预览卡片边距； 4. 移除冗余的 values-night 主题资源文件。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cn.alittlecookie.lut2photo.lut2photo"
         minSdk = 29
         targetSdk = 36
-        versionCode = 100032
-        versionName = "3.1.1"
+        versionCode = 100312
+        versionName = "3.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         renderscriptTargetApi = 21
